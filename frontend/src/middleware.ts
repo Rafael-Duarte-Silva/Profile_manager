b25fa69050473e66da551dbf59b07d67c9bbaa08
Rafael-Duarte-Silva <rafaelddduante@gmail.com>
2025-07-17 22:59:57 -0300

    fix: middleware default locale

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -9,7 +9,8 @@ export default async function middleware(request: NextRequest) {
     const token = request.cookies.get("jwt")?.value;
     const locale =
         request.nextUrl.pathname.split("/")[1] ||
-        request.cookies.get("NEXT_LOCALE")?.value;
+        request.cookies.get("NEXT_LOCALE")?.value ||
+        routing.defaultLocale;
 
     if (request.nextUrl.pathname.endsWith("/login")) {
         return intlMiddleware(request);
