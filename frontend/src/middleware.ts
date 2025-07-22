0b161ea14985ab609d48374573414cde3e7930ae
Rafael-Duarte-Silva <rafaelddduante@gmail.com>
2025-07-21 23:12:05 -0300

    fix: remove frontend middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,26 +1,8 @@
 import createMiddleware from "next-intl/middleware";
-import { NextRequest, NextResponse } from "next/server";
 
 import { routing } from "./i18n/routing";
 
-export const intlMiddleware = createMiddleware(routing);
-
-export default async function middleware(request: NextRequest) {
-    const token = request.cookies.get("jwt")?.value;
-    console.log(token);
-    const locale =
-        request.nextUrl.pathname.split("/")[1] ||
-        request.cookies.get("NEXT_LOCALE")?.value ||
-        routing.defaultLocale;
-
-    if (request.nextUrl.pathname.endsWith("/login"))
-        return intlMiddleware(request);
-
-    if (!token)
-        return NextResponse.redirect(new URL(`/${locale}/login`, request.url));
-
-    return intlMiddleware(request);
-}
+export default createMiddleware(routing);
 
 export const config = {
     matcher: ["/", "/(pt-BR|en)/:path*"],
